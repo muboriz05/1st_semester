12a9fc54ad24db9055bf01ed6be9516276b0f3d7
Muboriz <144531498+muboriz05@users.noreply.github.com>
2023-11-29 11:18:56 +0300

    Процедурное программирование

---
+++ HomeWork#2.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void task1() {
+    float r1, r2, h, l;
+    const float PI = 3.14;
+    cin >> r1 >> r2 >> h >> l;
+
+    if (r1 == r2 == h) {
+        cout << "Это не конус!";
+    }
+    else {
+        cout << "V: " << PI / 3 * h * (pow(r1, 2) + r1 * r2 + pow(r2, 2)) << endl;
+        cout << "S: " << PI * (pow(r1, 2) + r1 * r2 + pow(r2, 2)) << endl;
+    }
+}
+
+void task2() {
+    float x, a;
+    cin >> x >> a;
+    if (abs(x) < 1) {
+        cout << "w = " << a * log(abs(x));
+    }
+    else if (x = 0)
+        cout << "х не может быть равен 0";
+    else if (a - pow(x, 2) < 0)
+        cout << "Корень не должен быть отрицательным";
+    else
+        cout << "w = " << sqrt(a - pow(x, 2));
+}
+
+void task3() {
+    float x, y, b;
+    cin >> x >> y >> b;
+    if ((b - y < 1) || (b - x < 0))
+        cout << "Решения нет!";
+    else
+        cout << "z = " << log(b - y) * sqrt(b - x);
+}
+
+void task4() {
+    int n;
+    cin >> n;
+    for (int i = 0; i <= 10; i++) {
+        cout << n + i << ", ";
+    }
+    
+}
+
+void task5() {
+    /*
+    int k = 1;
+    for (float i = -4; i <= 4; i += 0.5) {
+        if (i == 1)
+            continue;
+        cout << "y" << k++ <<"= " << (pow(i, 2) - 2 * i + 2) / (i - 1) << endl;
+    }*/
+    int k = 1;
+    float x = -4;
+    while (x <= 4) {
+        if (x == 1)
+            continue;
+        cout << "y" << k++ << "= " << (pow(x, 2) - 2 * x + 2) / (x - 1) << endl;
+        x += 0.5;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    task5();
+}
+
---
+++ HomeWork#5.cpp
@@ -0,0 +1,180 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void task1();
+void task2();
+void task3();
+void task4();
+void task5();
+void task6();
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	task6();
+}
+
+//TASK1
+
+int gcdByDivision(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    else {
+        return gcdByDivision(b, a % b);
+    }
+}
+
+int gcdBySubtraction(int a, int b) {
+    while (a != b) {
+        if (a > b) {
+            a -= b;
+        }
+        else {
+            b -= a;
+        }
+    }
+    return a;
+}
+
+void task1(){
+    int num1, num2;
+    cout << "Введите два числа: ";
+    cin >> num1 >> num2;
+
+    
+    int resultByDivision = gcdByDivision(num1, num2);
+    cout << "НОД методом деления: " << resultByDivision << endl;
+
+    int resultBySubtraction = gcdBySubtraction(num1, num2);
+    cout << "НОД методом вычитания: " << resultBySubtraction << endl;
+
+}
+
+//TASK 2
+void task2() {
+    cout<<("Введите натуральное число: ");
+    int n;
+    cin>>n;
+
+    cout<<"Простые числа от 2 до "<< n<<": ";
+
+    for (int i = 2; i <= n; ++i) {
+        bool is_prime = true;
+
+        for (int j = 2; j * j <= i; ++j) {
+            if (i % j == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+
+        if (is_prime) {
+            cout<<i<< " ";
+        }
+    }
+
+ 
+}
+
+void task3() {
+    int t;
+    cout << "Введите натуральное число t < 27: ";
+    cin >> t;
+
+    if (t >= 27) {
+        cerr << "Ошибка: введенное число больше или равно 27." << endl;
+    }
+    else {
+        cout << "Трехзначные числа, сумма цифр которых равна " << t << ": ";
+
+        for (int i = 100; i <= 999; ++i) {
+            int digit_sum = 0;
+            int num = i;
+
+            while (num > 0) {
+                digit_sum += num % 10;
+                num /= 10;
+            }
+
+            if (digit_sum == t) {
+                cout << i << " ";
+            }
+        }
+    }
+}
+
+void task4() {
+    for (int i = 1000; i <= 9999; ++i) {
+        int num = i;
+        bool has_duplicate = false;
+
+        int digits[10] = { 0 };
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digits[digit] > 0) {
+                has_duplicate = true;
+                break;
+            }
+
+            digits[digit]++;
+            num /= 10;
+        }
+
+        if (!has_duplicate) {
+            cout << i << " ";
+        }
+    }
+}
+
+void task5() {
+        ifstream ifile("53.txt");
+        string str, w, strLen, d = " ";
+        string letter = "";
+        while (ifile.peek() >= 0) { 
+            ifile >> letter;
+            str = str + letter + " ";
+        }
+        ifile.close();
+
+        int b, e = 0;
+        while ((b = str.find_first_not_of(d, e)) != str.npos) {
+            e = str.find_first_of(d, b);
+            w = str.substr(b, e - b);
+            if (w.size() > strLen.size())
+                strLen = w;
+        }
+        cout << strLen << endl;
+
+    }
+    
+
+void task6() {
+    ifstream ifile("53.txt");
+    string word = "";
+    string result = "";
+    char letter = ' ';
+
+    cout << "Enter a word: ";
+    cin >> word;
+
+    while (ifile.peek() >= 0) { 
+        ifile >> letter;
+        result += letter;
+    }
+    ifile.close();
+
+    int pos = result.find(word, 0);
+
+    if (pos != -1) {
+        cout << "Position word: " << pos << endl;
+    }
+    else {
+        cout << "word not found...\n";
+    }
+}
